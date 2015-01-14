e4961def130706686254d2ed48f9e36400c6a459
Luke Jeter <lukas.jeter@streetlinks.com>
2015-01-14 00:15:26 -0600

    Grab Eventbright events by organizer_id.

--- CoderDojoMKE.Web/Scripts/site-upcoming-events.js
+++ CoderDojoMKE.Web/Scripts/site-upcoming-events.js
@@ -1,35 +1,35 @@
 ﻿var UpcomingEvents = function () {
 
     var loadEvents = function () {
-        $('.dojo-event').each(function (index, element) {
-            var eventId = $(this).data('eventbrite');
-            var eventUrl = 'https://www.eventbriteapi.com/v3/events/' + eventId + '/';
-            var that = this;
+        $.ajax({
+            method: 'GET',
+            url: 'https://www.eventbriteapi.com/v3/events/search',
+            data: {
+                'token': 'VXFP6CIOSA7NV6PAHWL5',
+                'organizer_id': '6894758817'
+            },
+            success: function (data, status, xhr) {
+                if (data.pagination.page_count == 1) {
+                    var futureEventList = $('#future-event-list')[0]; //lets assume there's only one
+                    $.each(data.events, function(index, event) {
+                        var formattedDate = event.start.local.split('T')[0].replace('-', '/');
 
-            $.ajax({
-                method: 'GET',
-                url: eventUrl,
-                data: {
-                    'token': 'VXFP6CIOSA7NV6PAHWL5'
-                },
-                success: function (data, status, xhr) {
-                    
-                    var eventDate = data.start.local.split('T');
-                    var eventDateParts = eventDate[0].split('-');
-                    var formattedDate = eventDateParts[1] + '/' + eventDateParts[2] + '/' + eventDateParts[0];
-                    var href = data.url;
-                    $(that).children('a.event-date-and-time').prop('href', href).html(data.name.text + ' ' + formattedDate);
-                    $.each(data.ticket_classes, function (index, element) {
-                        if (element.quantity_total - element.quantity_sold === 0) {
-                            $(that).append('<p>Sorry, this ticket type is sold out! Please try selecting a future event.</p>');
-                        } else {
-                            var available = element.quantity_total - element.quantity_sold;
-                            $(that).append('<p class="availability-info"><span class="number-available">' + available + ' ' + element.name + ' Session</span> tickets available</p>');
-                        }
+                        var li = futureEventList.append('<li>')
+                                    .append('<a class="event-date-and-time"')
+                                    .prop('href', event.url)
+                                    .html(data.name.text + ' ' + formattedDate);
+                        $.each(event.ticket_classes, function (index, element) {
+                            if (element.quantity_total - element.quantity_sold === 0) {
+                                li.append('<p>Sorry, this ticket type is sold out! Please try selecting a future event.</p>');
+                            } else {
+                                var available = element.quantity_total - element.quantity_sold;
+                                li.append('<p class="availability-info"><span class="number-available">' + available + ' ' + element.name + ' Session</span> tickets available</p>');
+                            }
+                        });
                     });
-                }
-            });
-        });        
+	            }
+            }
+        });
     };
 
     var _public = {};
